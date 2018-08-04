9b902c41f2d8ce940df329cb24d30a85d8934d33
Iqbal Fauzi <iqbalfawz@gmail.com>
2018-08-04 11:58:41 +0700

    Fix is_indexed_array function on ampty array

--- modules/core/helper/global.php
+++ modules/core/helper/global.php
@@ -66,6 +66,8 @@ function is_dev(): bool{
 }
 
 function is_indexed_array(array $array): bool{
+    if(!$array)
+        return true;
     return array_keys($array) === range(0, count($array) - 1);
 }
 
