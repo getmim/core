fa21faddfe89f54202567b5efb653eaaf9edb64e
Iqbal Fauzi <iqbalfawz@gmail.com>
2019-09-15 11:23:57 +0700

    Fix bug on logger before route

--- modules/core/library/Logger.php
+++ modules/core/library/Logger.php
@@ -37,7 +37,7 @@ class Logger {
         $path = BASEPATH . '/etc/log/error/' . date('/Y/m/d/h/') . uniqid() . '.txt';
         Fs::write($path, $tx);
 
-        if(\Mim::$app && \Mim::$app->req->gate){
+        if(\Mim::$app && isset(\Mim::$app->req) && \Mim::$app->req->gate){
             \Mim::$app->req->setProp('handler', \Mim::$app->req->gate->errors->{'500'}->_handlers);
             \Mim::$app->next();
         }
