ad7a3d518c99b21c150f7cd5f3042a1e7ed5904b
Iqbal Fauzi <iqbalfawz@gmail.com>
2026-04-16 06:39:56 +0700

    Add method getCondDateRange on service req

--- modules/core/service/Request.php
+++ modules/core/service/Request.php
@@ -16,7 +16,10 @@ class Request extends \Mim\Service
     private $_file_gates;
     private $_file_routes;
     
-    public function __construct(string $file_gates='/etc/cache/gates.php', string $file_routes='/etc/cache/routes.php'){
+    public function __construct(
+        string $file_gates = '/etc/cache/gates.php',
+        string $file_routes = '/etc/cache/routes.php'
+    ) {
         $this->_file_gates  = $file_gates;
         $this->_file_routes = $file_routes;
         
@@ -25,7 +28,8 @@ class Request extends \Mim\Service
         $this->_fillRoute();
     }
     
-    private function _fillGate(): void{
+    private function _fillGate(): void
+    {
         $result = Router::parseGate([
             'file_gate' => $this->_file_gates,
             'is_cli'    => $this->isCLI(),
@@ -33,58 +37,73 @@ class Request extends \Mim\Service
             'req_path'  => $this->path
         ]);
 
-        if(!$result)
+        if (!$result) {
             return;
+        }
 
         $this->_props['gate'] = $result['gate'];
-        foreach($result['param'] as $k => $val)
+        foreach ($result['param'] as $k => $val) {
             $this->_props['param']->$k = $val;
+        }
     }
     
-    private function _fillProps(): void{
+    private function _fillProps(): void
+    {
+        $user_aget = $this->getServer('HTTP_USER_AGENT');
+        $host = $this->getServer('HTTP_HOST');
+        $length = (int)$this->getServer('CONTENT_LENGTH', '0');
+        $method = $this->getServer('REQUEST_METHOD', 'GET');
+        $scheme = $this->getServer('REQUEST_SCHEME');
+        $content_type = $this->getServer('CONTENT_TYPE');
+        $uri = $this->getServer('REQUEST_URI');
+        $url = $scheme . '://' . $host . $uri;
+
         $this->_props = [
             'accept'    => null,
-            'agent'     => $this->getServer('HTTP_USER_AGENT'),
+            'agent'     => $user_aget,
             'body'      => null,
             'gate'      => null,
             'handler'   => null,
-            'host'      => $this->getServer('HTTP_HOST'),
+            'host'      => $host,
             'ip'        => null,
-            'length'    => (int)$this->getServer('CONTENT_LENGTH', '0'),
-            'method'    => $this->getServer('REQUEST_METHOD', 'GET'),
+            'length'    => $length,
+            'method'    => $method,
             'param'     => (object)[],
             'path'      => null,
             'route'     => null,
-            'scheme'    => $this->getServer('REQUEST_SCHEME'),
-            'type'      => $this->getServer('CONTENT_TYPE'),
-            'url'       => $this->getServer('REQUEST_SCHEME', 'http')
-                . '://' 
-                . $this->getServer('HTTP_HOST')
-                . $this->getServer('REQUEST_URI')
+            'scheme'    => $scheme,
+            'type'      => $content_type,
+            'url'       => $url
         ];
         
         // content type
         $type = $this->getServer('CONTENT_TYPE', '');
-        if($custom_type = $this->getQuery('__CONTENT_TYPE__', ''))
+        if ($custom_type = $this->getQuery('__CONTENT_TYPE__', '')) {
             $type = $custom_type;
-        if(strstr($type, ';'))
+        }
+        if (strstr($type, ';')) {
             $type = explode(';', $type)[0];
-        if(strstr($type, '/'))
+        }
+        if (strstr($type, '/')) {
             $type = explode('/', $type)[1];
+        }
         $this->_props['type'] = strtolower($type);
         
         // path
-        if($this->isCLI()){
+        if ($this->isCLI()) {
             $paths = $this->_getCLICommand();
             $this->_props['path'] = trim($paths);
-        }else{
-            $this->_props['path'] = trim('/' . trim(preg_replace('!\?.*$!', '', $this->getServer('REQUEST_URI')), '/'));
+        } else {
+            $paths = trim('/' . trim(preg_replace('!\?.*$!', '', $uri), '/'));
+            $this->_props['path'] = $paths;
         }
     }
     
-    private function _fillRoute(): void{
-        if(!$this->gate)
+    private function _fillRoute(): void
+    {
+        if (!$this->gate) {
             return;
+        }
 
         $result = Router::parseRoute([
             'file_routes' => $this->_file_routes,
@@ -96,23 +115,25 @@ class Request extends \Mim\Service
         ]);
 
         // find the route
-        if(!$result){
+        if (!$result) {
             $route = $this->gate->errors->{'404'};
             $this->_props['route'] = $route;
             $this->_props['handler'] = $route->_handlers;
-            
-        }else{
+        } else {
             $this->_props['route'] = $result['route'];
             $this->_props['handler'] = $result['route']->_handlers;
 
-            foreach($result['param'] as $k => $v)
+            foreach ($result['param'] as $k => $v) {
                 $this->_props['param']->$k = $v;
+            }
         }
     }
     
-    private function _getAccept(): object{
-        if(!is_null($this->_props['accept']))
+    private function _getAccept(): object
+    {
+        if (!is_null($this->_props['accept'])) {
             return $this->_props['accept'];
+        }
         
         // accept encoding
         $a_e = array_map('trim', explode(',', $this->getServer('HTTP_ACCEPT_ENCODING', '')));
@@ -122,10 +143,11 @@ class Request extends \Mim\Service
         
         // accept type
         $a_t = explode(',', $this->getServer('HTTP_ACCEPT', ''));
-        foreach($a_t as &$at){
+        foreach ($a_t as &$at) {
             $at = explode(';', trim($at))[0];
-            if(strstr($at, '/'))
+            if (strstr($at, '/')) {
                 $at = explode('/', $at)[1];
+            }
         }
         
         $this->_props['accept'] = (object)[
@@ -137,38 +159,41 @@ class Request extends \Mim\Service
         return $this->_props['accept'];
     }
 
-    private function _getCLICommand(): ?string{
-        if(!$this->isCLI())
+    private function _getCLICommand(): ?string
+    {
+        if (!$this->isCLI()) {
             return null;
+        }
 
         $commands = $_SERVER['argv'] ?? [];
-        if(!$commands)
+        if (!$commands) {
             return null;
+        }
 
-        if(preg_match('!index.php$!', $commands[0]))
+        if (preg_match('!index.php$!', $commands[0])) {
             array_shift($commands);
+        }
 
         // let's cleanup the command options
         $next_value = false;
-        foreach($commands as $index => $cmd){
-            if($cmd === '-'){
+        foreach ($commands as $index => $cmd) {
+            if ($cmd === '-') {
                 $next_value = false;
-
-            }elseif($cmd === '='){
+            } elseif ($cmd === '=') {
                 unset($commands[$index]);
                 $next_value = true;
-
-            }elseif(substr($cmd,0,1) === '-'){
+            } elseif (substr($cmd, 0, 1) === '-') {
                 unset($commands[$index]);
-                if(substr($cmd,1,1) === '-'){
-                    if(false === strstr($cmd, '='))
+                if (substr($cmd, 1, 1) === '-') {
+                    if (false === strstr($cmd, '=')) {
                         $next_value = true;
-                }else{
-                    if(strlen($cmd) == 2)
+                    }
+                } else {
+                    if (strlen($cmd) == 2) {
                         $next_value = true;
+                    }
                 }
-
-            }elseif($next_value){
+            } elseif ($next_value) {
                 unset($commands[$index]);
                 $next_value = false;
             }
@@ -177,27 +202,30 @@ class Request extends \Mim\Service
         return implode(' ', $commands);
     }
     
-    public function __get(string $name){
+    public function __get(string $name)
+    {
         $result = null;
-        switch($name){
-        case 'accept':
-            $result = $this->_getAccept();
-            break;
-        case 'body':
-            $result = $this->getBody();
-            break;
-        case 'ip':
-            $result = $this->getIP();
-            break;
-        default:
-            if(isset($this->_props[$name]))
-            $result = $this->_props[$name];
+        switch ($name) {
+            case 'accept':
+                $result = $this->_getAccept();
+                break;
+            case 'body':
+                $result = $this->getBody();
+                break;
+            case 'ip':
+                $result = $this->getIP();
+                break;
+            default:
+                if (isset($this->_props[$name])) {
+                    $result = $this->_props[$name];
+                }
         }
         
         return $result;
     }
     
-    public function forward(string $name, array $params=[]): void{
+    public function forward(string $name, array $params = []): void
+    {
         $c_gates  = include BASEPATH . $this->_file_gates;
         $c_routes = include BASEPATH . $this->_file_routes;
         
@@ -205,8 +233,8 @@ class Request extends \Mim\Service
         $gate_name = $gate_by_route->$name;
         
         // set the gate
-        foreach($c_gates as $gate){
-            if($gate->name === $gate_name){
+        foreach ($c_gates as $gate) {
+            if ($gate->name === $gate_name) {
                 $this->_props['gate'] = $gate;
                 break;
             }
@@ -218,8 +246,9 @@ class Request extends \Mim\Service
         
         // set the params
         $this->_props['param'] = (object)[];
-        foreach($params as $k => $v)
+        foreach ($params as $k => $v) {
             $this->_props['param']->$k = $v;
+        }
         
         // set handler
         $this->_props['handler'] = $route->_handlers;
@@ -228,8 +257,9 @@ class Request extends \Mim\Service
         $this->next();
     }
     
-    public function get(string $name=null, $def=null){
-        if(!is_null($name)){
+    public function get(string $name = null, $def = null)
+    {
+        if (!is_null($name)) {
             return $this->getQuery($name)
                 ?? $this->getPost($name)
                 ?? $this->getFile($name)
@@ -240,63 +270,126 @@ class Request extends \Mim\Service
         $result = [];
         
         $body = $this->getBody();
-        if(is_array($body) || is_object($body)){
-            foreach($body as $k => $v)
+        if (is_array($body) || is_object($body)) {
+            foreach ($body as $k => $v) {
                 $result[$k] = $v;
+            }
         }
         
-        foreach($_FILES as $k => $v)
+        foreach ($_FILES as $k => $v) {
             $result[$k] = $v;
+        }
         
-        foreach($_POST as $k => $v)
+        foreach ($_POST as $k => $v) {
             $result[$k] = $v;
+        }
         
-        foreach($_GET as $k => $v)
+        foreach ($_GET as $k => $v) {
             $result[$k] = $v;
+        }
         
         return $result;
     }
     
-    public function getBody(string $name=null, $def=null){
-        if(is_null($this->_props['body'])){
+    public function getBody(string $name = null, $def = null)
+    {
+        if (is_null($this->_props['body'])) {
             $body = file_get_contents('php://input');
-            if($this->type == 'json')
+            if ($this->type == 'json') {
                 $body = json_decode($body);
+            }
             $this->_props['body'] = $body;
         }
         
-        if(is_null($name))
+        if (is_null($name)) {
             return $this->_props['body'];
-        if(!is_object($this->_props['body']))
+        }
+        if (!is_object($this->_props['body'])) {
             return $def;
+        }
         return $this->_props['body']->$name ?? $def;
     }
 
-    public function getCond(array $queries): array{
+    public function getCond(array $queries): array
+    {
         $result = [];
-        foreach($queries as $query){
+        foreach ($queries as $query) {
             $value = \Mim::$app->req->getQuery($query);
-            if(is_null($value) || $value === '')
+            if (is_null($value) || $value === '') {
                 continue;
-            if(is_array($value))
+            }
+            if (is_array($value)) {
                 $value[] = '__!';
+            }
             $result[$query] = $value;
         }
 
         return $result;
     }
+
+    public function getCondDateRange(
+        string $ds = 'ds',
+        string $de = 'ds',
+        bool $time = true
+    ): ?array {
+        $cols = [
+            'ds' => null,
+            'de' => null
+        ];
+
+        foreach ($cols as $key => $value) {
+            $val = $this->getQuery($key);
+            if (!$val) {
+                continue;
+            }
+
+            $val = strtotime($val);
+            if (!$val) {
+                continue;
+            }
+
+            $val = date('Y-m-d', $val);
+            if ($time) {
+                $suffix = '00:00:00';
+                if ($key == 'de') {
+                    $suffix = '23:59:59';
+                }
+                $val .= ' ' . $suffix;
+            }
+
+            $cols[$key] = $val;
+        }
+
+        if ($cols['ds'] && $cols['de']) {
+            return ['__between', $cols['ds'], $cols['de']];
+        }
+
+        if ($cols['ds']) {
+            return ['__op', '>=', $cols['ds']];
+        }
+
+        if ($cols['de']) {
+            return ['__op', '<=', $cols['de']];
+        }
+
+        return null;
+    }
     
-    public function getCookie(string $name, string $def=null): ?string{
+    public function getCookie(string $name, string $def = null): ?string
+    {
         return $_COOKIE[$name] ?? $def;
     }
     
-    public function getFile(string $name): ?array{
+    public function getFile(string $name): ?array
+    {
         return $_FILES[$name] ?? null;
     }
     
-    public function getIP(): string{
-        if(!is_null($this->_props['ip']))
+    public function getIP(): string
+    {
+        if (!is_null($this->_props['ip'])) {
             return $this->_props['ip'];
+        }
         
         $this->_props['ip'] =
             $_SERVER['HTTP_CLIENT_IP'] ??
@@ -309,20 +402,24 @@ class Request extends \Mim\Service
         return $this->_props['ip'];
     }
 
-    public function getPager(int $rpp_default=12, int $rpp_max=24): array{
+    public function getPager(int $rpp_default = 12, int $rpp_max = 24): array
+    {
         $result = [
             'page' => (int)$this->getQuery('page', 1),
             'rpp'  => (int)$this->getQuery('rpp', $rpp_default)
         ];
 
-        if(!$result['page'] || $result['page'] < 1)
+        if (!$result['page'] || $result['page'] < 1) {
             $result['page'] = 1;
+        }
 
-        if(!$result['rpp'] || $result['rpp'] < 1)
+        if (!$result['rpp'] || $result['rpp'] < 1) {
             $result['rpp'] = $rpp_default;
+        }
 
-        if($result['rpp'] > $rpp_max)
+        if ($result['rpp'] > $rpp_max) {
             $result['rpp'] = $rpp_max;
+        }
 
         $result[0] = $result['page'];
         $result[1] = $result['rpp'];
@@ -330,39 +427,49 @@ class Request extends \Mim\Service
         return $result;
     }
     
-    public function getPost(string $name=null, $def=null){
-        if(is_null($name))
+    public function getPost(string $name = null, $def = null)
+    {
+        if (is_null($name)) {
             return $_POST;
+        }
         return $_POST[$name] ?? $def;
     }
     
-    public function getQuery(string $name=null, $def=null){
-        if(is_null($name))
+    public function getQuery(string $name = null, $def = null)
+    {
+        if (is_null($name)) {
             return $_GET;
+        }
         return $_GET[$name] ?? $def;
     }
     
-    public function getServer(string $name, string $def=null): ?string{
+    public function getServer(string $name, string $def = null): ?string
+    {
         return $_SERVER[$name] ?? $def;
     }
     
-    public function isAjax(): bool{
+    public function isAjax(): bool
+    {
         $header = $this->getServer('HTTP_X_REQUESTED_WITH');
         return $header && strtolower($header) === 'xmlhttprequest';
     }
     
-    public function isCLI(): bool{
+    public function isCLI(): bool
+    {
         return php_sapi_name() === 'cli';
     }
     
-    public function next(): void{
+    public function next(): void
+    {
         $handlers = $this->_props['handler'];
-        if(!$handlers)
+        if (!$handlers) {
             return;
+        }
         $handler = null;
-        foreach($handlers as $index => $hand){
-            if($hand->solved)
+        foreach ($handlers as $index => $hand) {
+            if ($hand->solved) {
                 continue;
+            }
             $handler = $hand;
             $hand->solved = true;
             $handlers[$index] = $hand;
@@ -370,7 +477,7 @@ class Request extends \Mim\Service
             break;
         }
         
-        if(!$handler){
+        if (!$handler) {
             \Mim::$app->res->send();
             return;
         }
@@ -382,7 +489,8 @@ class Request extends \Mim\Service
         $hdr->$method();
     }
     
-    public function setProp(string $name, $value): void{
+    public function setProp(string $name, $value): void
+    {
         // readonly props
         $readonly = [
             'accept',
@@ -401,7 +509,7 @@ class Request extends \Mim\Service
             'url'
         ];
         
-        if(in_array($name, $readonly)){
+        if (in_array($name, $readonly)) {
             trigger_error('Property ' . $name . ' is readonly');
             return;
         }
